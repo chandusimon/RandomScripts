563febfa73cc7c1d2b1a1d6ae03bac9aa3be2de3
simonsenshix <simonsenshix@proton.me>
2023-11-11 17:18:54 +0530

    SFTP_Mig_Win_Linux just comment

---
+++ Migration/SFTP_WindowsLocal_RemoteLinux.js
@@ -0,0 +1,7 @@
+/**
+ * When I was working with Teradata and Linux sh scripts, I mostly used this script to do the migration 
+ * instead of having to login to IPSwitch and manually do it myself typing every command 
+ * This was very helpful at the time because we had no local server to test teradata or linux scripts
+ * so even a single line code need to be pushed to server to be tested
+ */
+
